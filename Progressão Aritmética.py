49bfca9ac2affe37d043bbc1da00b0d2d71924eb
Mateus Correia <88751427+mateuscpg@users.noreply.github.com>
2022-01-03 23:18:09 -0300

    Add files via upload

--- Progressão Aritmética.py
+++ Progressão Aritmética.py
@@ -2,10 +2,26 @@ print ("="*20)
 print ("10 TERMOS DE UM PA")
 print ("="*20)
 
-prim = int(input("Primeiro termo:"))
-razão = int(input("Razão:"))
-décimo = prim + (10 - 1) * razão
+print (''' Você quer saber pelo:
+[1] FOR
+[2] WHILE''')
+op = int(input('Sua opção:'))
 
-for c in range (prim, décimo, razão):
-    print ('{}'.format (c), end = "->")
-print ("ACABOU")
+if op == 1:
+    prim = int(input("Primeiro termo:"))
+    razão = int(input("Razão:"))
+    décimo = prim + (10 - 1) * razão
+    for c in range (prim, décimo, razão):
+        print ('{}'.format (c), end = " -> ")
+    print ("ACABOU")
+    
+elif op == 2:
+    prim = int(input("Primeiro termo:"))
+    razão = int(input("Razão:"))
+    termo = prim
+    cont = 1
+    while cont <= 10:
+        print ('{}'.format (termo), end = " -> ")
+        termo += razão
+        cont += 1
+    print ("ACABOU")
